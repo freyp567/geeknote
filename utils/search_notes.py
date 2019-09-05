c91a74a7959cf843c9f7c019017d5b3ec85e3d8b
freyp567 <freyp567@gmail.com>
2019-09-05 11:49:04 +0200

    extend search tests

--- utils/search_notes.py
+++ utils/search_notes.py
@@ -8,7 +8,8 @@ from geeknote import config
 import sys
 import os
 import argparse
-import re 
+import re
+from datetime import datetime
 
 import logging
 
@@ -45,25 +46,58 @@ class SearchNote:
 
     def _clean_uri_for_logging(self, uri):
         """ dont show password """
-        uri2 = re.sub("(mongodb://.+?)\:(.+?)@(.+)", r"\1@\3", uri)
+        uri2 = re.sub("(mongodb://.+?)\:(.+?)@(.+)", r"\1@\3", uri)  # noqa: W605
         return uri2
 
-    def search(self, keywords):
+    def search_title(self, keywords):
+        query = {"Title": {"$regex": ".*%s.*" % re.escape(keywords)}}
+        start = datetime.now()
+        result = self.db.notes.find(query)
+        duration = datetime.now() - start
+        result_count = result.count()
+        # TODO verify cause:
+        # bson.errors.InvalidStringData: strings in documents must be valid UTF-8
+        if result_count:
+            LOGGER.info('found %s notes (in titles) dT=%.2f', 
+                        result.count(), duration.total_seconds())
+            for note in result:
+                LOGGER.info('+ "%s"', note["Title"])
+        else:
+            LOGGER.info('no notes found for "%s" (in note titles)', keywords)
+        search_info = {
+            'field': 'Title',
+            'term': keywords,
+            'count': result_count,
+            'duration': duration.total_seconds()
+        }
+        return search_info
+
+    def search_content(self, keywords):
         LOGGER.info('searching for "%s"', keywords)
         # db.note_contents.createIndex( { name: "text", description: "note fulltext" } )
         # query = {"$text": {"contents": keywords}}
 
         # for beginning, use contains search
-        # query = {"content": {"$regex": ".*%s.*" % re.escape(keywords)}}
+        # query = {"Content": {"$regex": ".*%s.*" % re.escape(keywords)}}
         # result = self.db.note_contents.find(query)
 
-        query = {"Titel": {"$regex": ".*%s.*" % re.escape(keywords)}}
-        result = self.db.notes.find(query)
-        LOGGER.info('found %s notes', result.count()) 
-        # TODO show timing
-        for note in result:
-            note = note
-        return
+        query = {"content": {"$regex": ".*%s.*" % re.escape(keywords)}}
+        result = self.db.note_contents.find(query)
+        result_count = result.count()
+        if result_count:
+            LOGGER.info('found %s notes (in content)', result_count)
+            # TODO show timing
+            for note in result:
+                note = note
+        else:
+            LOGGER.info('no notes found for "%s" (in note contents)', keywords)
+        search_info = {
+            'field': 'Citle',
+            'term': keywords,
+            'count': result_count,
+            'duration': duration.total_seconds()
+        }
+        return search_info
 
 
 def get_argparser():
@@ -78,12 +112,18 @@ def main():
     # args = parser.parse_args()
 
     search_note = SearchNote()
+    # TODO search terms (and expectation) from external config
     for search_term in (
         # 'Karawane',
         # u'Alexander der Große'
-        'Haefs',
-        ):
-        search_note.search(search_term)
+        'Haefs',  # expect 4 notes
+        u'Gablé',  # expect 17 notes
+        'Rose',  # expect 51 notes
+        'Marco Polo: Bis ans Ende der Welt',  # expect 6 notes (fulltext), 1 note (exact)
+        'Die Hüter der Rose – Wikipedia',  # expect 1 note
+    ):
+        search_note.search_title(search_term)
+        # search_note.search_content(search_term)
 
     # TODO show tabular output of search term, duration, hits
 
